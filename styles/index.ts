d2159fc8d33337d495a79b3ae0e85d602d533475
Vinay Negi <vinegi004@gmail.com>
2026-04-02 13:24:00 +0530

    Intial Commit

---
+++ styles/index.ts
@@ -0,0 +1,14 @@
+import { colors, fontSize } from "@/constants/token"
+import { StyleSheet } from "react-native"
+
+const defaultStyle=StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:colors.background
+    },
+    text:{
+        fontSize:fontSize.base,
+        color:colors.text
+    }
+})
+
